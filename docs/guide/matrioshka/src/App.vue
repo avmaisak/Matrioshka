5e0ab8c24f99b0fedc42a81ba8f3c13beeb03ce9
Anton Maisak <am@rus-bit.com>
2018-05-14 13:37:08 +0300

    added utils, doc to vue.js.(see docs folder)

---
+++ docs/guide/matrioshka/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header class="clr-bg-indigo-500 p-2"></header>
+    <main >
+      <div class="rw">
+        <div class="cl w-2 h-100 clr-bg-grey-200 p-t-1 p-l-2 clr-indigo-800">
+           <a><router-link to="/">Главная</router-link></a>
+        </div>
+        <div class="cl w-10 ">
+          <div class="p-t-1 p-l-2 p-r-3 p-b-4">
+            <router-view/>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss" src="../../../../src/scss/matrioshka.complete.scss"></style>
+<style lang="css" src="../../../../dist/icons/css/matrioshkaIcons.min.css"></style>
---
+++ docs/guide/matrioshka/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="txt-c">
+  <div class="clr-indigo-800">
+    <h1>{{title}}</h1>
+    <h3>{{msg}}</h3>
+  </div>
+    <img src="../../../../../misc/design/logo/matreshka.svg" width="240" alt="matreshka" class="m-t-3 m-b-2" />
+    <p class="p-l-10 p-r-10 m-t-4 txt-j">{{desc}}</p>
+    <a href="https://github.com/avmaisak/Matrioshka" class="f-sz-2 clr-grey-500 d-inline-block m-t-3"><i class="mi-github"></i></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      title: 'Матрёшка',
+      msg: 'Модульный HTML5/CSS3 каркас',
+      desc: 'Матрёшка позволяет быстро прототипировать Ваши идеи, и воплощать их в жизнь. Благодаря модульной системе, Вы экономите место и используете только те компоненты, которые Вам необходимы в каждом конкретном случае.'
+    }
+  }
+}
+</script>

6afb1b3288b680ee2aa673f2a137939ee8e5c20d
Anton Maisak <am@rus-bit.com>
2018-05-18 11:50:27 +0300

    fix w-$(media) width units

--- docs/guide/matrioshka/src/components/Grid.vue
+++ docs/guide/matrioshka/src/components/Grid.vue
@@ -2,24 +2,24 @@
   <div>
     <h1 class="clr-indigo-600 m-b-2">Сетка</h1>
     <div class="rw h-3 m-b-2">
-        <div class="cl w-1 clr-bg-green-50">1</div>
-        <div class="cl w-1 clr-bg-green-100">2</div>
-        <div class="cl w-1 clr-bg-green-200">3</div>
-        <div class="cl w-1 clr-bg-green-300">4</div>
-        <div class="cl w-1 clr-bg-green-400">5</div>
-        <div class="cl w-1 clr-bg-green-500">6</div>
-        <div class="cl w-1 clr-bg-green-600">7</div>
-        <div class="cl w-1 clr-bg-green-700">8</div>
-        <div class="cl w-1 clr-bg-green-800">9</div>
-        <div class="cl w-1 clr-bg-green-900">10</div>
-        <div class="cl w-1 clr-bg-green-800">11</div>
-        <div class="cl w-1 clr-bg-green-700">12</div>
+        <div class="cl w-1 clr-bg-green-50"></div>
+        <div class="cl w-1 clr-bg-green-100"></div>
+        <div class="cl w-1 clr-bg-green-200"></div>
+        <div class="cl w-1 clr-bg-green-300"></div>
+        <div class="cl w-1 clr-bg-green-400"></div>
+        <div class="cl w-1 clr-bg-green-500"></div>
+        <div class="cl w-1 clr-bg-green-600"></div>
+        <div class="cl w-1 clr-bg-green-700"></div>
+        <div class="cl w-1 clr-bg-green-800"></div>
+        <div class="cl w-1 clr-bg-green-900"></div>
+        <div class="cl w-1 clr-bg-green-800"></div>
+        <div class="cl w-1 clr-bg-green-700"></div>
     </div>
 
     <div class="rw h-3">
-        <div class="cl w-3 clr-bg-blue-50">3</div>
-        <div class="cl w-4 clr-bg-blue-100">4</div>
-        <div class="cl w-5 clr-bg-blue-200">5</div>
+        <div class="cl w-3-lg w-2-md w-3-sm w-4-smx clr-bg-blue-50"></div>
+        <div class="cl w-4-lg w-2-md w-3-sm w-4-smx clr-bg-blue-100"></div>
+        <div class="cl w-5-lg w-2-md w-3-sm w-4-smx clr-bg-blue-200"></div>
     </div>
   </div>
 </template>
--- docs/guide/matrioshka/src/router/index.js
+++ docs/guide/matrioshka/src/router/index.js
@@ -7,6 +7,7 @@ import Grid from '@/components/Grid'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',

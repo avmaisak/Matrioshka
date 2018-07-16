f024303e1be620ae120567b9471fcfd5868b82d6
Anton Maisak <am@rus-bit.com>
2018-07-16 22:45:36 +0300

    u 2017.07.16

--- tools/gulp/gulpfile.js
+++ tools/gulp/gulpfile.js
@@ -1,6 +1,6 @@
-﻿
-//gulp
+﻿//gulp
 var
+	fs = require('fs'),
 	gulp = require('gulp'),
 	cleanCSS = require('gulp-clean-css'),
 	rename = require('gulp-rename'),
@@ -12,49 +12,57 @@ var
 	sass = require('gulp-sass'),
 	consolidate = require('gulp-consolidate'),
 	iconfont = require('gulp-iconfont'),
-	svgSprite = require('gulp-svg-sprite')
+	svgSprite = require('gulp-svg-sprite'),
+	svg_to_png = require('svg-to-png')
+// convertFile = require('convert-svg-to-png')
 ;
+
 
 //gulp common config options
 var gulpCfg = {
+	srcRoot: "../../src/",
 	scssDestRoot: "../../src/scss/",
-	distFileDestination:"../../dist/css/"
+	distRoot: "../../dist/",
+	distFileDestination: "../../dist/css/"
 };
 
+// очистить консоль
 console.log('\x1Bc');
 
-gulp.task('clean', ()=> {
+// очистить каталог с файлами production
+gulp.task('clean', () => {
 	return gulp.src(`${gulpCfg.distFileDestination}*`)
-		.pipe(clean({force: true}));
+		.pipe(clean({
+			force: true
+		}));
 });
-
-gulp.task('clean-icons', ()=> {
+// очистить каталог с иконками для production
+gulp.task('clean-icons', () => {
 	return gulp.src('../../dist/icons/*')
-		.pipe(clean({force: true}));
+		.pipe(clean({
+			force: true
+		}));
 });
-
+// выполнить очистку
 gulp.task('clean-all', gulp.series([
 	'clean',
 	'clean-icons'
 ]));
-
+// запуск препроцессора SASS
 gulp.task('sass', () => {
 	return gulp.src(`${gulpCfg.scssDestRoot}*.scss`)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(`${gulpCfg.scssDestRoot}`))
-	;
+		.pipe(gulp.dest(`${gulpCfg.scssDestRoot}`));
 });
-
+// переместить сгенерированные файлы css в каталог для production
 gulp.task('css-all', () => {
 	return gulp.src("../../src/scss/*.css")
-
 		.pipe(gulp.dest(`../../dist/css`));
 });
 
 gulp.task('css-all-min', () => {
 	return gulp.src("../../src/scss/*.css")
 		.pipe(cleanCSS())
-
 		.pipe(rename({
 			suffix: ".min",
 			extname: ".css"
@@ -64,16 +72,19 @@ gulp.task('css-all-min', () => {
 
 gulp.task('css-icons-min', () => {
 	return gulp.src("../../dist/icons/css/matrioshkaIcons.css")
-		.pipe(cleanCSS({ allowEmpty: true }))
+		.pipe(cleanCSS({
+			allowEmpty: true
+		}))
 		.pipe(rename("matrioshkaIcons.min.css"))
 		.pipe(gulp.dest('../../dist/icons/css/'));
 });
 
 gulp.task('css-all-tarball', () => {
-
 	gzip({
 		append: true,
-		gzipOptions: { level: 9 }
+		gzipOptions: {
+			level: 9
+		}
 	});
 
 	return gulp.src("../../dist/css/min/*.css")
@@ -83,32 +94,32 @@ gulp.task('css-all-tarball', () => {
 
 gulp.task('iconfont', function () {
 	return gulp.src('../../src/icons/svg/*.svg')
-		 .pipe(iconfont({
-			 fontName: 'matrioshkaIcons',
-			 formats: ['ttf', 'eot', 'woff', 'woff2','svg'],
-			 appendCodepoints: true,
-			 appendUnicode: false,
-			 normalize: true,
-			 fontHeight: 1001,
-			 centerHorizontally: true
-		 }))
-		 .on('glyphs', function (glyphs, options) {
-			 gulp.src('../../tools/icons/matrioshkaIcons.css')
-				 .pipe(consolidate('underscore', {
-					 glyphs: glyphs,
-					 fontName: options.fontName,
-					 fontDate: new Date().getTime()
-				 }))
-				 .pipe(gulp.dest('../../dist/icons/css/'));
+		.pipe(iconfont({
+			fontName: 'matrioshkaIcons',
+			formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
+			appendCodepoints: true,
+			appendUnicode: false,
+			normalize: true,
+			fontHeight: 1001,
+			centerHorizontally: true
+		}))
+		.on('glyphs', function (glyphs, options) {
+			gulp.src('../../tools/icons/matrioshkaIcons.css')
+				.pipe(consolidate('underscore', {
+					glyphs: glyphs,
+					fontName: options.fontName,
+					fontDate: new Date().getTime()
+				}))
+				.pipe(gulp.dest('../../dist/icons/css/'));
 
-			 gulp.src('../../tools/icons/matrioshkaIcons.html')
-				 .pipe(consolidate('underscore', {
-					 glyphs: glyphs,
-					 fontName: options.fontName
-				 }))
-				 .pipe(gulp.dest('../../dist/icons/'));
-		 })
-		 .pipe(gulp.dest('../../dist/icons/fonts/'));
+			gulp.src('../../tools/icons/matrioshkaIcons.html')
+				.pipe(consolidate('underscore', {
+					glyphs: glyphs,
+					fontName: options.fontName
+				}))
+				.pipe(gulp.dest('../../dist/icons/'));
+		})
+		.pipe(gulp.dest('../../dist/icons/fonts/'));
 });
 
 gulp.task('make-icons', gulp.series([
@@ -117,32 +128,53 @@ gulp.task('make-icons', gulp.series([
 	'css-icons-min'
 ]));
 
-gulp.task('make-icon-sprite', function (){
-// More complex configuration example
-	config= {
+gulp.task('make-icon-sprite', function () {
+	// More complex configuration example
+	config = {
 		shape: {
-			dimension: {			// Set maximum dimensions
+			dimension: { // Set maximum dimensions
 				maxWidth: 64,
 				maxHeight: 64
 			},
-			spacing: {			// Add padding
+			spacing: { // Add padding
 				padding: 20
 			},
 		},
 		mode: {
-			view: {			// Activate the «view» mode
+			view: { // Activate the «view» mode
 				bust: false,
 				render: {
-					scss: false		// Activate Sass output (with default options)
+					scss: false // Activate Sass output (with default options)
 				}
 			},
-			symbol: false		// Activate the «symbol» mode
+			symbol: false // Activate the «symbol» mode
 		}
 	};
 
-	return  gulp.src('**/*.svg',{cwd: '../../src/icons/svg'})
+	return gulp.src('**/*.svg', {
+			cwd: '../../src/icons/svg'
+		})
 		.pipe(svgSprite(config))
 		.pipe(gulp.dest('../../dist/icons/sprite/'));
+});
+
+gulp.task('svgtopng', () => {
+	fs.readdir(`${gulpCfg.srcRoot}icons/svg`, (err, data) => {
+		if (err) throw err;
+		data.forEach(d => {
+			console.log(d);
+
+			// console.log(d);
+			var inputFilePath = `${gulpCfg.srcRoot}icons/svg/${d}`;
+			var outputFilePath = `${gulpCfg.distRoot}icons/i/png/${d.replace('.svg','')}.png`;
+			svg_to_png.convert(inputFilePath, outputFilePath) // async, returns promise
+			 	.then(function (a) {
+					console.log(a)
+			 	});
+
+
+		});
+	});
 });
 
 gulp.task('default', gulp.series([
